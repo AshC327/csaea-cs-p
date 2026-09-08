8829fd63f376256acdb66bb4e6f7632994c73dba
AshC327 <ash.coop327@gmail.com>
2026-09-08 18:22:25 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,27 @@
+# comment
+print ("word")
+
+# Variable declarations and data types
+
+a = 4 #interger
+b = 5.5 #float
+c = "CSAEA" #sting
+d = False #boolean
+
+print (c, a, b, d)
+
+#operators
+# + - / *   % ** //
+
+# += -= /=
+
+e = 8 // 8
+
+#f-string
+
+print (f"e is equal to {e}")
+
+e -= 7
+e += 12
+
+print (f"e is now equal to {e}")
