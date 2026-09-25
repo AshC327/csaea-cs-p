c71a8a76cc1c33dd20712dfa375ebafee68fa94d
AshC327 <ash.coop327@gmail.com>
2026-09-25 18:25:47 +0000

    dont trust 7

--- crash_course_challenges.py
+++ crash_course_challenges.py
@@ -1,6 +1,6 @@
 import math
 
-#make a list starting with apples, and includes cheese and rice
+#10-------------------------------------------------
 
 groceries = ["milk","eggs", "bread"]
 
@@ -12,7 +12,7 @@ groceries.append("rice")
 
 print(groceries)
 
-#-------------------------------------------------
+#3-------------------------------------------------
 
 f = 212
 x = f-32
@@ -20,7 +20,7 @@ c = x/1.8
 
 print(c)
 
-#--------------------------------------------------
+#4--------------------------------------------------
 
 g = 84
 
@@ -35,7 +35,7 @@ elif g >= 60:
 else:
     print("F")
 
-#--------------------------------------------------
+#5--------------------------------------------------
 
 p = "csaea2026"
 r = "csaea2026"
@@ -45,5 +45,44 @@ if r == p:
     print("Access Granted")
 else:
     print("Access Denied")
+
+#20----------------------------------------------------
+
+sl = 55
+s = 71
+
+if s > sl:
+    print("Fine: $100")
+else:
+    print("Speed limit: okay")
+
+#7----------------------------------------------------
+
+h = 50
+a = 8
+ha = True
+
+hh = h>=48
+aa = a<10
+
+if hh is True:
+    hh = True
+else:
+    hh = False
+
+if aa is True:
+    aa = True
+else:
+    aa = False
+
+if ha is aa:
+    z = True
+else:
+    z = False
+
+if hh == aa == z:
+    print("You may ride")
+else:
+    print("You may not ride")
 
 #----------------------------------------------------
